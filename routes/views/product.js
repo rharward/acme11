dcc9e017b7ea1d220ac2f57aaa20e1659bfd6951
Reid Harward <reid.harward@gmail.com>
2018-04-25 09:15:25 -0400

    First commit

---
+++ routes/views/product.js
@@ -0,0 +1,29 @@
+var keystone = require('keystone');
+exports = module.exports = function(req,res) {
+
+  var view = new keystone.View(req,res);
+  var locals = res.locals;
+
+  //setlocals
+  locals.section = 'store';
+  locals.filters = {
+    product: req.params.product
+  }
+  locals.data = {
+    products:[]
+  }
+  //Load Products
+  view.on('init', function(next){
+    var q = keystone.list('Product').model.findOne({
+      slug: locals.filters.product
+    });
+
+    q.exec(function(err,result){
+      locals.data.product = result;
+      next(err);
+    //  next();
+    });
+  });
+  //Render View
+  view.render('product');
+}
